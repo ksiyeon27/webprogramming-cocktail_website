dcd25566a723d3dbccdb3aff6e7cc799471a2fa7
ksiyeon27 <siyeonk27@gmail.com>
2021-06-03 18:50:26 +0000

    중복 삭제

--- app/models/cocktail.rb
+++
@@ -1,9 +0,0 @@
-class Cocktail < ApplicationRecord
-    # serialize :tags, Array
-    # serialize :flavor, Array
-    # serialize :base, Array
-    
-    validates :name,
-	 	 	 presence: true,
-	 	 	 length: { minimum: 1 } ##예시로 한번..
-end
