4ec7cb35eb3f208fef88b75522766cdfc43c9367
hna0608 <hna0608@gmail.com>
2021-06-07 13:23:17 +0000

    signup/login done

--- CocktailPages/config/routes.rb
+++ CocktailPages/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'account/index'
+
+  get 'accounts/index'
+
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     get 'cocktails/search'
     root 'cocktails#search'
