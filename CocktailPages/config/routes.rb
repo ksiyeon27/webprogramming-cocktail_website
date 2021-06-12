b0ca71f5e074c4501d728f45d558a5796e35ca97
ChaelynnLynnKim <cyberlynn1@gmail.com>
2021-06-12 08:22:31 +0000

    임시

--- CocktailPages/config/routes.rb
+++ CocktailPages/config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     get 'cocktails/search'
     root 'cocktails#search'
+    
+    get 'cocktails/recommend'
+    get 'cocktails/search_result'
+    
     resources :cocktails do
       resources :comments
     end
