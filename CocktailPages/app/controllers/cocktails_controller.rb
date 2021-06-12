28f7667a59b0a928a134627c2d2a00c4904e2b16
yeppin <yebins49@gmail.com>
2021-06-12 15:07:52 +0000

    feat adding cocktail list page

--- CocktailPages/app/controllers/cocktails_controller.rb
+++ CocktailPages/app/controllers/cocktails_controller.rb
@@ -33,6 +33,7 @@ class CocktailsController < ApplicationController
         elsif params[:format] == "unlike"
             @cocktail.unliked_by current_user
         end
+        redirect_to controller: 'cocktails', action: 'show'
     end    
 private
 
